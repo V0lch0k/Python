85e3aed3ab6832de85c3db9934bcf171e5a5bd11
V0lch0k <33514637+V0lch0k@users.noreply.github.com>
2021-03-15 13:00:13 +1100

    Update sequence.py

--- sequence.py
+++ sequence.py
@@ -6,17 +6,25 @@ x[1]= 1.5
 sumx = 0
 sum23x = 0
 
+#print initial values
 for i in range(0,2,1):
         print(x[i])
 
+#calculate next number in sequence and add it to the array
+#round to 7 decimal places and print value
 for i in range(2,10,1):
         x[i] = 0.2 * x[i-1] + 1.2 * x[i-2]
         print(np.round(x[i],7))
 
+#calculate sum of values that are between 2 and 3
 for i in range(0,10,1):
         if(2 < x[i] <3):
                 sum23x = sum23x + x[i]
 
+#calculate total sum
 sumx = np.sum(x)
+
+#print total sum
+#print sum of values between 2 and 3
 print(np.round(sumx,7))
 print(np.round(sum23x,7))
